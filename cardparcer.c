bbdb81232d9d7ae3ed746c43958d951128fb1b74
Bredian <sasha.kry@gmail.com>
2018-04-07 20:53:31 +0300

    Refined parcer and updated NR Defs

--- cardparcer.c
+++ cardparcer.c
@@ -7,7 +7,7 @@ int main(int argc, char * argv[]){
 	printf("Cardparcer launched\n");
 	char  name[100];
 	char  text[20];
-	int value;
+	int kek,value[3],skill[3],heroic,strength,row,faction;
 	FILE * library;
 	FILE * code;
 	library = fopen(argv[1],"r");
@@ -20,46 +20,53 @@ int main(int argc, char * argv[]){
 		printf("Initialising card...\n");
 		fscanf(library,"%s",name);
 		printf("Card name read\n");
-		fprintf(code,"card %s;\n",name);
 		printf("Card name written\n");
 		fscanf(library,"%s",text);
 		printf("Card identy read\n");
 		if(strcmp(text,"leader")==0)
-			fprintf(code,"%s.leadership=1;\n%s.speciality=0;\n%s.unit=0;\n",name,name,name);
+			value[0]=1;
+            value[1]=0;
+            value[2]=0;
 		if(strcmp(text,"special")==0)
-			fprintf(code,"%s.leadership=0;\n%s.speciality=1;\n%s.unit=0;\n",name,name,name);
+			value[0]=0;
+            value[1]=1;
+            value[2]=0;
 		if(strcmp(text,"unit")==0)
-			fprintf(code,"%s.leadership=0;\n%s.speciality=0;\n%s.unit=1;\n",name,name,name);
+			value[0]=0;
+            value[1]=0;
+            value[2]=1;
 		printf("Card identity written\n");
-		fscanf(library,"%d",&value);
+		fscanf(library,"%d",&kek);
 		printf("Card skill read\n");
         	if(strcmp(text,"leader")==0)
-			fprintf(code,"%s.leader_skill=%d;\n%s.special_skill=0;\n%s.unit_skill=0;\n",name,value,name,name);
+			skill[0]=kek;
+            skill[1]=0;
+            skill[2]=0;
 		if(strcmp(text,"special")==0)
-			fprintf(code,"%s.leader_skill=0;\n%s.special_skill=%d;\n%s.unit_skill=0;\n",name,name,value,name);
+			skill[0]=0;
+            skill[1]=kek;
+            skill[2]=0;
 		if(strcmp(text,"unit")==0)
-			fprintf(code,"%s.leader_skill=0;\n%s.special_skill=0;\n%s.unit_skill=%d;\n",name,name,name,value);
+			skill[0]=0;
+            skill[1]=0;
+            skill[2]=kek;
 		printf("Card skill written\n");
-		fscanf(library,"%d",&value);
+		fscanf(library,"%d",&heroic);
 		printf("Card heroism read\n");
-		fprintf(code,"%s.heroic=%d;\n",name,value);
 		printf("Card heroism written\n");
-		fscanf(library,"%d",&value);
+		fscanf(library,"%d",&strength);
 		printf("Card strength read\n");
-		fprintf(code,"%s.strength=%d;\n",name,value);
 		printf("Card strength written\n");
-		fscanf(library,"%d",&value);
+		fscanf(library,"%d",&row);
 		printf("Card row read\n");
-		fprintf(code,"%s.row=%d;\n",name,value);
 		printf("Card row written\n");
-		fscanf(library,"%d",&value);
+		fscanf(library,"%d",&faction);
 		printf("Card faction read\n");
-		fprintf(code,"%s.faction=%d;\n",name,value);
 		printf("Card faction written\n");
 		fscanf(library,"%s",text);
 		printf("Card type read\n");
-		fprintf(code,"%s.type=\"%s\";\n",name,text);
 		printf("Card type written\n");
+        fprintf(code,"volatile card %s={%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,\"%s\"};\n",name,value[0],value[1],value[2],skill[0],skill[1],skill[2],heroic,strength,row,faction,text);
 	}
 	return 0;
 }
