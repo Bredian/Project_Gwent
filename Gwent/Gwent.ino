0b97b85f79b86577e0a4ea43ea30e2d19bd46951
Bredian <sasha.kry@gmail.com>
2018-04-07 20:54:39 +0300

    Arduino Gwent init

---
+++ Gwent/Gwent.ino
@@ -0,0 +1,27 @@
+
+typedef struct card{
+  int leadership;
+  byte speciality;
+  byte unit;
+  byte leader_skill;
+  byte special_skill;
+  byte unit_skill;
+  byte heroic;
+  byte strength;
+  byte row;
+  byte faction;
+  char * type;
+} card;
+
+#include "library.h"
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
